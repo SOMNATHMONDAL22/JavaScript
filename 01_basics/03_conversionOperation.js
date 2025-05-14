6b66a8cd8d4944da27cb8d6efd69160eac5f1fe2
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-14 18:44:26 +0000

    Conversion_opperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,59 @@
-let score = "somnath"
+// let score = "somnath"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number(score)
-console.log(valueInNumber,typeof valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(valueInNumber,typeof valueInNumber)
 
-let newScore = 45
-console.log(String(newScore) , typeof newScore)
+// let newScore = 45
+// console.log(String(newScore) , typeof newScore)
 
-// "33 => 33"
-// "33abc" => NaN 
-// true => 1 , false =>0
+// // "33 => 33"
+// // "33abc" => NaN 
+// // true => 1 , false =>0
 
-let isLoggedIn = 54646
+// let isLoggedIn = 54646
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true , 0=> false
-// "" => false , "somnath" => true
+// // 1 => true , 0=> false
+// // "" => false , "somnath" => true
 
 
-let someNumber = 22;
-let stringNumber = String(someNumber)
-console.log(stringNumber, typeof stringNumber)
+// let someNumber = 22;
+// let stringNumber = String(someNumber)
+// console.log(stringNumber, typeof stringNumber)
+
+// ***************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+// console.log(2/2)
+
+// let str1 = "hello"
+// let str2 = "somnath"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+3+"2")
+// console.log("1"+3+2)
+
+console.log(+true) //1
+console.log(+"") // false
+
+let num1 ,num2,num3
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter)
