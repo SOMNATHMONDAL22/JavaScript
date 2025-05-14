4634a90c6e11de7410ad1d9a77a91223a0222c4e
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-14 18:19:16 +0000

    Conversion of datatyeps

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "somnath"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber,typeof valueInNumber)
+
+let newScore = 45
+console.log(String(newScore) , typeof newScore)
+
+// "33 => 33"
+// "33abc" => NaN 
+// true => 1 , false =>0
+
+let isLoggedIn = 54646
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true , 0=> false
+// "" => false , "somnath" => true
+
+
+let someNumber = 22;
+let stringNumber = String(someNumber)
+console.log(stringNumber, typeof stringNumber)
