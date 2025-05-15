13641cf2632223b23618f3927141cfdfadf427b1
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-15 06:33:11 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = 'Somnath'
+const repocount = 50
+// console.log(name+repocount+"Value")
+
+// console.log(`Hello my name is ${name} and my repocount is ${repocount}`)
+
+const gameName = new String('Somnath-Mondal-sm') 
+
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+
+// // console.log(gameName.length)
+// // console.log(gameName.toUpperCase())
+// // console.log(gameName.charAt(5))
+// // console.log(gameName.indexOf('h'))
+
+// const newString =  gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(newString)
+
+const newString1  = "    Somnath   "
+console.log(newString1)
+console.log(newString1.trim())
+
+const url = "https://somnath.com/somnath%20mondal"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('somnath'))
+console.log(gameName.split("-"))
