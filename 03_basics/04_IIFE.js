174d7097f161379c7b1854f7e677a2f9fa0c1bf5
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-16 14:59:52 +0000

    IIFE

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,10 +1,21 @@
 //Immediately Invoked Function Expression( IIFE ) > IIFE is used when there is pollution in global scope, so to remove the pollution of global scope and varibales , we use IIFE. Syntax is mentioned below.
 
 (function chai(){
+    //named IIFE
     console.log("DB conencted");
 })();
                              
 
-(()=>{
+((name)=>{
     console.log("DB conencted2");
-})()
+})('Somnath')
+
+//Javascript execution contexts :
+
+//1> Global execution context : >is always there.
+//2> Function execution context
+//3> Eval execution context
+
+//A> Memory creation phase
+//B> execution phase
+
