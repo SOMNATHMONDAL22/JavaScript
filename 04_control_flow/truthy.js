45b756846c2aca5800d83f44cb6dbb931317b4eb
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-17 04:59:51 +0000

    truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,3 +1,44 @@
-const userEmail = "som@gmail.ai"
+// const userEmail = []
 
-if()
+// if(userEmail) { //truthy value
+//     console.log("User has got email");
+// }
+// else{
+//     console.
+//     log(" User don't have a mail")
+// }
+//falsy values
+// false, 0,-0,BigInt, 0n,"",null,undefined,NaN
+
+//truthy value :
+//"0",'false'," ",[],{},function()
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if(Object.key(emptyObj).length === 0){ //Object.key changes it to an array 
+//     console.log("Object is empty");
+// }
+
+//Nullish coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+var1 = undefined ?? 15
+val2 = null ?? 10 ??15
+
+console.log(val1)
+console.log(val1)
+
+//Ternary operator
+
+// condition ? true : false 
+
+const iceTeaPrice =100
+iceTeaPrice >=90 ? console.log("More than 90"): console.log("Less than 90");
+
