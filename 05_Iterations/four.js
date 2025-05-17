038c0f6068cc964139d9a72beecc491e3886e0db
Somnath Mondal <91214306+SOMNATHMONDAL22@users.noreply.github.com>
2025-05-17 07:19:27 +0000

    for_in/off

---
+++ 05_Iterations/four.js
@@ -0,0 +1,17 @@
+const myObject  = {
+    js : 'javascript',
+    cpp: 'CSS',
+    rd:'ruby',
+    swift:'swift by apple'
+}
+
+// for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+
+// }
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    console.log(`${key} key is for ${programming[key]}`);
+}
